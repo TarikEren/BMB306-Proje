98e8e208049c6c23bf02af5fffbb0a858d7f0bfa
TarikEren <1210606015@nku.edu.tr>
2024-03-18 02:10:15 +0300

    Proje dizin oluşturma

---
+++ backend/src/test/java/com/personal/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="">Hello, World!</div>
+    </>
+  )
+}
+
+export default App
