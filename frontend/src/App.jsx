19f7d1d262f0b301ec7e16b618c351897be4abb2
TarikEren <1210606015@nku.edu.tr>
2024-03-24 16:50:29 +0300

    Takvim

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import Calendar from './Components/Calendar';
 function App() {
   const mali = "ANNEN KENDİNİ ASTI AAAĞAĞHĞAĞHĞAHĞAĞ"
   return (
     <>
       <div className="">{mali}</div>
+      <Calendar />
     </>
   )
 }
