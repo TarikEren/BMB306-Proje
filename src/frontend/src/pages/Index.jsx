61be5c4b53452f350a03be82d3bdf685eabaf3f7
TarikEren <1210606015@nku.edu.tr>
2024-05-18 20:28:29 +0300

    Ana sayfa eklendi

---
+++ src/frontend/src/pages/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Index() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div>Index</div>
+    </React.Fragment>
+  );
+}
+
+export default Index;
