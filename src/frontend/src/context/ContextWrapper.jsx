b9e84224f36318ad402d47ad1f81bec127605c8e
TarikEren <1210606015@nku.edu.tr>
2024-05-18 21:12:21 +0300

    Context düzenlendi

--- src/frontend/src/context/ContextWrapper.jsx
+++ src/frontend/src/context/ContextWrapper.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import GlobalContext from "./GlobalContext";
 
-return <GlobalContext.Provider
-    value={
+function ContextWrapper(props) {
+    return (
+        <GlobalContext.Provider>
+            {props.children}
+        </GlobalContext.Provider>
+    );
+}
 
-    }>
-
-</GlobalContext.Provider>
+export default ContextWrapper;
