3351b302fef25fdb0db81646a2740e0df684e78c
TarikEren <1210606015@nku.edu.tr>
2024-05-18 20:27:40 +0300

    React-router-dom kuruldu, ana sayfa eklendi

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,10 +1,23 @@
+//Modules
+import React, { useState, useEffect } from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
+
+//Pages
+import Index from './pages/Index';
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Index />
+    }
+])
 function App() {
 
-  return (
-    <>
-      <div className="bg-red-700 text-white rounded p-2 font-semibold">Hello, World!</div>
-    </>
-  )
+    return (
+        <>
+            <div className="bg-red-700 text-white rounded p-2 font-semibold">Hello, World!</div>
+        </>
+    )
 }
 
 export default App
