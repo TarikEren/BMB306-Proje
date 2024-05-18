addf54c8e510c9f79fa755fe35c830bd73dd1333
TarikEren <1210606015@nku.edu.tr>
2024-05-19 01:33:53 +0300

    Context eklendi

--- src/frontend/src/context/ContextWrapper.jsx
+++ src/frontend/src/context/ContextWrapper.jsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, {
+    useState,
+    useEffect,
+    useMemo,
+} from "react";
 import GlobalContext from "./GlobalContext";
+import dayjs from "dayjs";
 
-function ContextWrapper(props) {
+export default function ContextWrapper(props) {
+    const [monthIndex, setMonthIndex] = useState(dayjs().month());
+    const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
+    const [daySelected, setDaySelected] = useState(dayjs());
+    const [showEventModal, setShowEventModal] = useState(false);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [labels, setLabels] = useState([]);
+    const [allEvents, setAllEvents] = useState([]);
+
+    const filteredEvents = useMemo(() => {
+        return allEvents.filter((evt) =>
+            labels
+                .filter((lbl) => lbl.checked)
+                .map((lbl) => lbl.label)
+                .includes(evt.label)
+        );
+    }, [allEvents, labels]);
+
+    useEffect(() => {
+        if (smallCalendarMonth !== null) {
+            setMonthIndex(smallCalendarMonth);
+        }
+    }, [smallCalendarMonth]);
+
+    useEffect(() => {
+        if (!showEventModal) {
+            setSelectedEvent(null);
+        }
+    }, [showEventModal]);
+
+
     return (
-        <GlobalContext.Provider>
+        <GlobalContext.Provider
+            value={{
+                monthIndex,
+                setMonthIndex,
+                smallCalendarMonth,
+                setSmallCalendarMonth,
+                daySelected,
+                setDaySelected,
+                showEventModal,
+                setShowEventModal,
+                selectedEvent,
+                setSelectedEvent,
+                allEvents,
+                setAllEvents,
+                setLabels,
+                labels,
+                filteredEvents,
+            }}
+        >
             {props.children}
         </GlobalContext.Provider>
     );
 }
-
-export default ContextWrapper;
--- src/frontend/src/context/GlobalContext.jsx
+++ src/frontend/src/context/GlobalContext.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
-const GlobalContext = React.createContext({});
+const GlobalContext = React.createContext({
+    monthIndex: 0,
+    setMonthIndex: (index) => { },
+    smallCalendarMonth: 0,
+    setSmallCalendarMonth: (index) => { },
+    daySelected: null,
+    setDaySelected: (day) => { },
+    showEventModal: false,
+    setShowEventModal: () => { },
+    selectedEvent: null,
+    allEvents: [],
+    setAllEvents: () => { },
+    setSelectedEvent: () => { },
+    setLabels: () => { },
+    labels: [],
+    filteredEvents: [],
+});
 
 export default GlobalContext;
