268753a97c48895cc5ec830f890616809225eec5
TarikEren <1210606015@nku.edu.tr>
2024-05-19 23:13:01 +0300

    Düzenleme

--- src/frontend/src/pages/Admin.jsx
+++ src/frontend/src/pages/Admin.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 
 function Index() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="h-screen w-screen">
-        <div className="grid grid-cols-7 h-screen">
-            Admin dashboard
-        </div>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Navbar />
+            <div className="h-screen w-screen">
+                Admin dashboard
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default Index;
