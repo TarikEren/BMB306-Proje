711e7ea9c4f0f843a7ee2952d3f9d2574e9d7c75
TarikEren <1210606015@nku.edu.tr>
2024-05-19 20:11:57 +0300

    Premium kontrollü linkler eklendi

--- src/frontend/src/components/CalendarHeader.jsx
+++ src/frontend/src/components/CalendarHeader.jsx
@@ -5,7 +5,8 @@ import GlobalContext from "../context/GlobalContext";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 export default function CalendarHeader() {
-  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  const { monthIndex, setMonthIndex, userIsPremium }
+  = useContext(GlobalContext);
   function handlePrevMonth() {
     setMonthIndex(monthIndex - 1);
   }
@@ -20,32 +21,42 @@ export default function CalendarHeader() {
     );
   }
   return (
-    <header className="px-4 py-2 flex items-center bg-red-400 text-white">
-      <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
-      <h1 className="mr-10 text-xl text-white fond-bold">
-        Calendar
-      </h1>
-      <button
-        onClick={handleReset}
-        className="border rounded py-2 px-4 mr-5"
-      >
-        Today
-      </button>
-      <button onClick={handlePrevMonth} className="p-3 mr-1 border rounded">
-        <span className="material-icons-outlined cursor-pointer">
-          <FaChevronLeft/>
-        </span>
-      </button>
-      <button onClick={handleNextMonth} className="p-3 ml-1 border rounded">
-        <span className="material-icons-outlined cursor-pointer">
-          <FaChevronRight/>
-        </span>
-      </button>
-      <h2 className="ml-4 text-xl text-white font-bold">
-        {dayjs(new Date(dayjs().year(), monthIndex)).format(
-          "MMMM YYYY"
+    <header className="px-4 py-2 flex items-center bg-red-400 text-white justify-between">
+      <div className="flex flex-row items-center">
+        <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
+        <h1 className="mr-10 text-xl text-white fond-bold">
+          Calendar
+        </h1>
+        <button
+          onClick={handleReset}
+          className="border rounded py-2 px-4 mr-5"
+        >
+          Today
+        </button>
+        <button onClick={handlePrevMonth} className="p-3 mr-1 border rounded">
+          <span className="material-icons-outlined cursor-pointer">
+            <FaChevronLeft />
+          </span>
+        </button>
+        <button onClick={handleNextMonth} className="p-3 ml-1 border rounded">
+          <span className="material-icons-outlined cursor-pointer">
+            <FaChevronRight />
+          </span>
+        </button>
+        <h2 className="ml-4 text-xl text-white font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format(
+            "MMMM YYYY"
+          )}
+        </h2>
+      </div>
+      <div className="flex flex-row items-center space-x-7">
+        {userIsPremium ? (
+          <a href="/plans" className="p-3 border rounded">Plans</a>
+        ) : (
+          <a href="/payment" className="p-3 border rounded">Buy Premium</a>
         )}
-      </h2>
+        <a href="/account" className="p-3 border rounded">Account</a>
+      </div>
     </header>
   );
 }
