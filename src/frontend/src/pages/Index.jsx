23e50a10e14ff6b7b63f28ab812b7d80834ac479
TarikEren <1210606015@nku.edu.tr>
2024-05-19 16:28:04 +0300

    Layout

--- src/frontend/src/pages/Index.jsx
+++ src/frontend/src/pages/Index.jsx
@@ -5,7 +5,16 @@ function Index() {
   return (
     <React.Fragment>
       <Navbar />
-      <div>Index</div>
+      <div className="h-screen w-screen">
+        <div className="grid grid-cols-7">
+          <div className="col-span-2">
+            Login
+          </div>
+          <div className="col-span-5">
+            Resim
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
