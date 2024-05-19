f1a055061d37057cb9d95f383dec99fbe4b55fbf
TarikEren <1210606015@nku.edu.tr>
2024-05-19 20:04:53 +0300

    Türkçeleştirildi

--- src/frontend/src/util.js
+++ src/frontend/src/util.js
@@ -1,15 +1,18 @@
 import dayjs from "dayjs";
+import "dayjs/locale/tr";
+
+dayjs.locale("tr");
 
 export function getMonth(month = dayjs().month()) {
-  month = Math.floor(month);
-  const year = dayjs().year();
-  const firstDayOfTheMonth = dayjs(new Date(year, month, 0)).day();
-  let currentMonthCount = 0 - firstDayOfTheMonth;
-  const daysMatrix = new Array(5).fill([]).map(() => {
-    return new Array(7).fill(null).map(() => {
-      currentMonthCount++;
-      return dayjs(new Date(year, month, currentMonthCount));
+    month = Math.floor(month);
+    const year = dayjs().year();
+    const firstDayOfTheMonth = dayjs(new Date(year, month, 0)).day();
+    let currentMonthCount = 0 - firstDayOfTheMonth;
+    const daysMatrix = new Array(5).fill([]).map(() => {
+        return new Array(7).fill(null).map(() => {
+            currentMonthCount++;
+            return dayjs(new Date(year, month, currentMonthCount));
+        });
     });
-  });
-  return daysMatrix;
+    return daysMatrix;
 }
