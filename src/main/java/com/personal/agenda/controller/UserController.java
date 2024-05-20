a39575b06418054f00c9c0c2b8802f365ce210be
MaliErdgn <malierdgnn@gmail.com>
2024-05-20 18:00:54 +0300

    Name-Surname-Email Eklendi

--- src/main/java/com/personal/agenda/controller/UserController.java
+++ src/main/java/com/personal/agenda/controller/UserController.java
@@ -12,31 +12,39 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
     @GetMapping("/{id}")
-    public User getUserDetails(@PathVariable Long id) {
+    public User getUserById(@PathVariable Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
-    @PutMapping("/{id}")
-    public User updateUserDetails(@PathVariable Long id, @RequestBody User userDetails) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user != null) {
-            //Yeni kullanıcı adı daha önceden alınmadıysa değiştir
-            user.setUsername(userDetails.getUsername());
-            user.setPassword(userDetails.getPassword());
-            user.setAccountType(userDetails.getAccountType());
-            return userRepository.save(user);
-        }
-        return null;
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 
-    @PutMapping("/{id}/change-password")
-    public User changePassword(@PathVariable Long id, @RequestBody String newPassword) {
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
         User user = userRepository.findById(id).orElse(null);
         if (user != null) {
-            user.setPassword(newPassword);
+            user.setAccountType(userDetails.getAccountType());
+            user.setUsername(userDetails.getUsername());
+            user.setPassword(userDetails.getPassword());
+            user.setName(userDetails.getName());
+            user.setSurname(userDetails.getSurname());
+            user.setEmail(userDetails.getEmail());
             return userRepository.save(user);
+        } else {
+            return null;
         }
-        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
     }
 }
