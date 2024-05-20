7d8b5ca0792e98d5bb51db3ac1db15a510386e2e
Ali Yıldız <06.alyildiz.06@gmail.com>
2024-05-20 16:29:10 +0300

    login finished

--- src/frontend/src/pages/Index.jsx
+++ src/frontend/src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import { FaUser } from "react-icons/fa"
+import { FaUser,FaLock } from "react-icons/fa"
 import  "./css/login.css"
 
 function Index() {
@@ -9,18 +9,17 @@ function Index() {
             <form action="">
               <h1>Login</h1>
               <div className="input-box">
-                <input type="text" placeholder='Username' required />
-                <FaUser />
+                <input type="text" placeholder='Username' required /><FaUser className='icon'/>
               </div>
               <div className="input-box">
-                <input type="password" placeholder='Password' required />
+                <input type="password" placeholder='Password' required /><FaLock className='icon'/>
               </div>
 
               <div className='remember-forgot'>
-                <label><input type="checkbox"/>RememberMe</label>
+                <label><input type="checkbox"/>Remember Me</label>
                 <a href="#">Forgot Password</a>
               </div>
-              <button type='submit'>Login</button>
+              <button type="submit">Login</button>
               
               <div className="register-link">
                 <p>Hala bir hesabın yok mu? <a href="#">Register</a></p>
