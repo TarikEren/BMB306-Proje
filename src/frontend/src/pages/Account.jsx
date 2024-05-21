80e88ff2466ad47554c6c972ba1ac5064064b3ff
MaliErdgn <malierdgnn@gmail.com>
2024-05-21 16:40:38 +0300

    account yorum

--- src/frontend/src/pages/Account.jsx
+++ src/frontend/src/pages/Account.jsx
@@ -135,7 +135,7 @@ function Account() {
           </div>
           <div className=" m-4 text-xl">
             {" "}
-            <p className="mb-3">Premium admin veya free user bilgisi</p>
+            {/* <p className="mb-3">Premium admin veya free user bilgisi</p> */}
             <a
               className="transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-md text-[#fef2f2]"
               href="/payment"
