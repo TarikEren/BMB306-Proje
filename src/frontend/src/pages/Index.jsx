3b66749aacc125cfe6b9d0779cca4cd047fcb7f7
TarikEren <1210606015@nku.edu.tr>
2024-05-21 19:52:06 +0300

    Formatting

--- src/frontend/src/pages/Index.jsx
+++ src/frontend/src/pages/Index.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { FaUser,FaLock } from "react-icons/fa"
-import  "./css/login.css"
+import { FaUser, FaLock } from "react-icons/fa"
+import "./css/login.css"
 
 function Index() {
-  return ( <div className='bodyy'>
+  return (
+    <div className='bodyy'>
       <div className="wrapper">
-            <form action="">
-              <h1>Login</h1>
-              <div className="input-box">
-                <input type="text" placeholder='Username' required /><FaUser className='icon'/>
-              </div>
-              <div className="input-box">
-                <input type="password" placeholder='Password' required /><FaLock className='icon'/>
-              </div>
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder='Username' required /><FaUser className='icon' />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder='Password' required /><FaLock className='icon' />
+          </div>
 
-              <div className='remember-forgot'>
-                <label><input type="checkbox"/>Remember Me</label>
-                <a href="#">Forgot Password</a>
-              </div>
-              <button type="submit">Login</button>
-              
-              <div className="register-link">
-                <p>Hala bir hesabın yok mu? <a href="/register">Register</a></p>
-              </div>
-            </form>
+          <div className='remember-forgot'>
+            <label><input type="checkbox" />Remember Me</label>
+            <a href="#">Forgot Password</a>
           </div>
+          <button type="submit">Login</button>
+
+          <div className="register-link">
+            <p>Hala bir hesabın yok mu? <a href="/register">Register</a></p>
           </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
