2883d4ef875c5644af0a554cd8c99c310f060a13
Fatih-Atalay <1210606018@nku.edu.tr>
2024-05-21 16:02:34 +0300

    plan bas

--- src/frontend/src/pages/Plans.jsx
+++ src/frontend/src/pages/Plans.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
+
 function Plans() {
   return (
     <React.Fragment>
       <Navbar/>
-      <div>Plans</div>
+      <div class="w-screen h-screen m-10 ">
+      <div class="row h-screen">
+  <div class="col-sm-6 h-screen">
+    <div class="card h-9/12">
+      <div class="card-body ">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 h-screen">
+    <div class="card ">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+     
+</div>
     </React.Fragment>
   );
 }
