6e9a40f6b4288cc61573d73745b105b654f366ca
MaliErdgn <malierdgnn@gmail.com>
2024-05-21 16:48:34 +0300

    stil düzeltme

--- src/frontend/src/pages/Plans.jsx
+++ src/frontend/src/pages/Plans.jsx
@@ -15,8 +15,8 @@ function Plans() {
             <div class="card h-9/12">
               <div class="card-body ">
                 <h5 class="card-title">Vip Paketi</h5>
-                <p class="card-text"> -Sınırsız randevu hakkıı </p>
-                <p class="card-text">7/24 Destek hattı</p>
+                <p class="card-text">- Sınırsız randevu hakkıı </p>
+                <p class="card-text">- 7/24 Destek hattı</p>
                 <p class="card-text">-  35 TL </p>
                 <br></br>
                 <br></br>
@@ -28,10 +28,10 @@ function Plans() {
             <div class="card ">
               <div class="card-body">
                 <h5 class="card-title">Destekçi Paketi</h5>
-                <p class="card-text"> -Destek icindir vip den farkı yoktur  </p>
-                <p class="card-text"> -Sınırsız ranevu hakkı </p>
-                <p class="card-text">-7/24 Destek hattı</p>
-                <p class="card-text">-60 TL</p>
+                <p class="card-text">- Destek icindir vip den farkı yoktur  </p>
+                <p class="card-text">- Sınırsız ranevu hakkı </p>
+                <p class="card-text">- 7/24 Destek hattı</p>
+                <p class="card-text">- 60 TL</p>
                 <br></br>
                 <Link to="/payment" class="w-7/12 btn btn-primary" onClick={() => { setPrice(60) }}>Satın al</Link>
               </div>
