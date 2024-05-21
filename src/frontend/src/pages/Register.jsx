627978982067566372f97921c94d243f42994acf
Ali Yıldız <06.alyildiz.06@gmail.com>
2024-05-21 16:03:41 +0300

    register

--- src/frontend/src/pages/Register.jsx
+++ src/frontend/src/pages/Register.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { FaUser,FaLock } from "react-icons/fa"
+import  "./css/login.css"
 
 function Register() {
   return (
-    <React.Fragment>
-      <Navbar/>
-      <div>Register</div>
-    </React.Fragment>
+    <div className='bodyy'>
+      <div className="wrapper">
+            <form action="">
+              <h1>Register</h1>
+              <div className="input-box">
+                <input type="text" placeholder='Username' required /><FaUser className='icon'/>
+              </div>
+              <div className='person'>
+              <div className="input-box">
+                <input type="text" placeholder='Name' required />
+              </div>
+              <div className="input-box">
+                <input type="text" placeholder='Surname' required />
+              </div>
+              </div>
+              <div className="input-box">
+                <input type="password" placeholder='Password' required /><FaLock className='icon'/>
+              </div>
+              <div className="input-box">
+                <input type="password" placeholder='Re-enter Password' required /><FaLock className='icon'/>
+              </div>
+              <button type="submit"className='btn'>Save</button>
+
+            </form>
+          </div>
+          </div>
   );
 }
 
