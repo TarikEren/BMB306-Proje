7df4adeeecff110d2213df94afb33497285f1fa0
MaliErdgn <malierdgnn@gmail.com>
2024-05-21 16:15:37 +0300

    About eklendi

--- src/frontend/src/pages/About.jsx
+++ src/frontend/src/pages/About.jsx
@@ -5,7 +5,27 @@ function About() {
   return (
     <>
       <Navbar />
-      <div>About</div>
+      <div class="text-center text-4xl">BMB-306 Proje Ödevi</div>
+      <ul>
+        <li>
+        <p class="text-center mt-3 text-2xl">Mehmet Ali Erdoğan</p>
+        </li>
+        <li>
+        <p class="text-center mt-3 text-2xl">Tarik Eren Tosun</p>
+        </li>
+        <li>
+        <p class="text-center mt-3 text-2xl">Mert Çalışkan</p>
+        </li>
+        <li>
+        <p class="text-center mt-3 text-2xl">Mert Hasgeçkin</p>
+        </li>
+        <li>
+        <p class="text-center mt-3 text-2xl">Ali Yıldız</p>
+        </li>
+        <li>
+        <p class="text-center mt-3 text-2xl">Fatih Atalay</p>
+        </li>
+      </ul>
     </>
   );
 }
