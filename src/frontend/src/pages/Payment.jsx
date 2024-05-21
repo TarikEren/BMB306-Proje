5dbe14a9748f22ef6245040043d18a53df375ca9
TarikEren <1210606015@nku.edu.tr>
2024-05-21 13:14:36 +0300

    Kontroller ve responsive dizayn eklendi

--- src/frontend/src/pages/Payment.jsx
+++ src/frontend/src/pages/Payment.jsx
@@ -10,24 +10,43 @@ function Payment() {
     const [cardNumber, setCardNumber] = useState(null);
     const [validCardNumber, setValidCardNumber] = useState(null);
     const [cardOwner, setCardOwner] = useState(null);
+    const [validCardOwner, setValidCardOwner] = useState(null);
+
     const checkValidOwner = () => {
-        let hasNoDigits = /^[a-zA-Z]+$/.test(cardOwner);
+        const ownerBox = document.getElementById("card-owner");
+        let hasNoDigits = !/[^a-z]/i.test(cardOwner);
         if (cardOwner !== null && hasNoDigits) {
+            setValidCardOwner(true);
+            ownerBox.classList.remove("border-red-500");
             return true;
         }
+        ownerBox.classList.add("border-red-500");
         return false;
     }
     const checkValidNumber = () => {
+        const numberBox = document.getElementById("card-number");
         let hasNoLetters = /^\d+$/.test(cardNumber);
-        if (cardNumber !== null && hasNoLetters && cardNumber.length === 16) return true;
+        if (cardNumber !== null && hasNoLetters && cardNumber.length === 16) {
+            setValidCardNumber(true);
+            numberBox.classList.remove("border-red-500");
+            return true;
+        };
+        numberBox.classList.add("border-red-500");
+        setValidCardNumber(false);
+        return false;
+    }
+    const checkValidity = () => {
+        if (checkValidNumber() && checkValidOwner()) {
+            return true;
+        }
         return false;
     }
     function sendPayment() {
-        if (checkValidNumber() && checkValidOwner()) {
+        if (checkValidity()) {
             //Sorun yoksa kullanıcıyı premium yap
             return true;
         }
-        return null;
+        return false;
     }
     return (
         <React.Fragment>
@@ -35,7 +54,7 @@ function Payment() {
             {/* Wrapper */}
             <div className="h-screen w-screen">
                 {/* Container */}
-                <div className="flex flex-row justify-center mt-10 space-x-4">
+                <div className="flex sm:flex-col lg:flex-row lg:justify-center mt-10 lg:space-x-4">
                     <div className="">
                         <div className="border w-12/12 p-5 space-y-10">
                             <header className='flex flex-col'>
@@ -54,32 +73,46 @@ function Payment() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="pl-1 flex flex-row items-center justify-between">
+                            <div className="pl-1 flex flex-row items-center">
                                 <label>
-                                    <h1 className="text-xl font-semibold">
-                                        Kredi kartı
+                                    <h1 className="text-xl font-semibold mr-2">
+                                        Kart
                                     </h1>
                                 </label>
-                                <select name="" id="" className="bg-white border rounded w-10/12 p-3">
+                                <select name="" id="" className="bg-white border rounded w-full p-3 ml-16">
                                     <option value="visa">Visa</option>
                                     <option value="mastercard">Mastercard</option>
                                 </select>
                             </div>
-                            <div className="pl-1 flex flex-row items-center justify-between">
+                            <div className="pl-1 flex flex-row items-center">
                                 <label>
                                     <h1 className="text-xl font-semibold">
                                         Kart Sahibi
                                     </h1>
                                 </label>
-                                <input type="text" name="" id="" className="border rounded w-10/12 p-3" />
+                                <div className="flex flex-col w-full">
+                                    <input type="text" name="" id="card-owner" className="border rounded p-3 ml-6" onChange={(e) => setCardOwner(e.target.value)}/>
+                                    {validCardOwner === false && (
+                                        <div className="">
+                                            <p className='text-gray-500'>Geçersiz kart sahibi</p>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                            <div className="pl-1 flex flex-row items-center justify-between">
+                            <div className="pl-1 flex flex-row items-center">
                                 <label>
                                     <h1 className="text-xl font-semibold">
                                         Kart Numarası
                                     </h1>
                                 </label>
-                                <input type="text" name="" id="" className="border rounded w-10/12 p-3" onChange={(e) => setCardNumber(e.target.value)} />
+                                <div className="flex flex-col w-full">
+                                    <input type="text" name="" id="card-number" className="border rounded p-3" onChange={(e) => setCardNumber(e.target.value)} />
+                                    {validCardNumber === false && (
+                                        <div className="">
+                                            <p className='text-gray-500'>Geçersiz kart numarası</p>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                             <div className="pl-1 flex flex-row items-center space-x-10">
                                 <div className="flex flex-col space-y-3">
@@ -109,7 +142,7 @@ function Payment() {
                             </div>
                         </div>
                     </div>
-                    <div className="border w-12/12 p-5 h-fit w-3/12 space-y-5">
+                    <div className="border p-5 h-fit lg:w-3/12 sm:w-full space-y-5">
                         <h1 className='text-2xl font-semibold'>
                             Sipariş Özeti
                         </h1>
@@ -119,7 +152,9 @@ function Payment() {
                         <p className='text-2xl font-semibold'>Genel Toplam: TL</p>
                         <button
                             className='p-2 rounded border w-full bg-green-400 text-white font-semibold text-lg'
-                            onClick={sendPayment}>
+                            onClick={() => {
+                                sendPayment()
+                            }}>
                             <div className="flex flex-row items-center justify-center space-x-3">
                                 <p>
                                     Devam Et
