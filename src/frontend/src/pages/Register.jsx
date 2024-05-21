4e6f7903bbd16e0e1545658473ebab23c5b308f5
TarikEren <1210606015@nku.edu.tr>
2024-05-21 19:50:52 +0300

    Post request eklendi

--- src/frontend/src/pages/Register.jsx
+++ src/frontend/src/pages/Register.jsx
@@ -1,37 +1,75 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { FaUser,FaLock } from "react-icons/fa"
-import  "./css/login.css"
+import React, { useEffect, useState } from 'react';
+import { FaUser, FaLock } from "react-icons/fa"
+import "./css/login.css"
+import axios from "axios";
+
 
 function Register() {
-  return (
-    <div className='bodyy'>
-      <div className="wrapper">
-            <form action="">
-              <h1>Register</h1>
-              <div className="input-box">
-                <input type="text" placeholder='Username' required /><FaUser className='icon'/>
-              </div>
-              <div className='person'>
-              <div className="input-box">
-                <input type="text" placeholder='Name' required />
-              </div>
-              <div className="input-box">
-                <input type="text" placeholder='Surname' required />
-              </div>
-              </div>
-              <div className="input-box">
-                <input type="password" placeholder='Password' required /><FaLock className='icon'/>
-              </div>
-              <div className="input-box">
-                <input type="password" placeholder='Re-enter Password' required /><FaLock className='icon'/>
-              </div>
-              <button type="submit"className='btn'>Save</button>
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    
+    const getAllUsers = async () => {
+        const users = await axios.get("http://localhost:8080/user")
+            .then((res) => console.log(res.data))
+            .catch((err) => console.error(err));
+    }
 
-            </form>
-          </div>
-          </div>
-  );
+
+    const sendUser = async () => {
+        await axios.post("http://localhost:8080/user", {
+            accountType: "free",
+            email: email,
+            username: username,
+            password: password,
+            name: name,
+            surname: surname
+        })
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err));
+    }
+
+    function handleRegister() {
+        sendUser();
+    }
+
+    useEffect(() => {
+        getAllUsers();
+    }, [])
+    return (
+        <div className='bodyy'>
+            <div className="wrapper">
+                <form action="">
+                    <h1>Register</h1>
+                    <div className="input-box">
+                        <input type="email" placeholder='Email' required onChange={(e) => setEmail(e.target.value)} /><FaUser className='icon' />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder='Username' required onChange={(e) => setUsername(e.target.value)} /><FaUser className='icon' />
+                    </div>
+                    <div className='person'>
+                        <div className="input-box">
+                            <input type="text" placeholder='Name' required onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="input-box">
+                            <input type="text" placeholder='Surname' required onChange={(e) => setSurname(e.target.value)} />
+                        </div>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder='Password' required onChange={(e) => setPassword(e.target.value)} /><FaLock className='icon' />
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder='Re-enter Password' required onChange={(e) => setConfirmPassword(e.target.value)} /><FaLock className='icon' />
+                    </div>
+                    <button type="button" className='btn' onClick={() => handleRegister()}>Save</button>
+
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Register;
