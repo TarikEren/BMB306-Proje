df1d7051773f939f25911ddb213f12916c95de52
MaliErdgn <malierdgnn@gmail.com>
2024-05-21 16:21:35 +0300

    Abouta numaralar eklendi

--- src/frontend/src/pages/About.jsx
+++ src/frontend/src/pages/About.jsx
@@ -8,22 +8,23 @@ function About() {
       <div class="text-center text-4xl">BMB-306 Proje Ödevi</div>
       <ul>
         <li>
-        <p class="text-center mt-3 text-2xl">Mehmet Ali Erdoğan</p>
+        <p class="text-center mt-3 text-2xl">Mehmet Ali Erdoğan 1210606004</p>
         </li>
         <li>
-        <p class="text-center mt-3 text-2xl">Tarik Eren Tosun</p>
+        <p class="text-center mt-3 text-2xl">Tarik Eren Tosun 1210606015</p>
         </li>
         <li>
-        <p class="text-center mt-3 text-2xl">Mert Çalışkan</p>
+        <p class="text-center mt-3 text-2xl">Mert Çalışkan 1200606059</p>
         </li>
         <li>
-        <p class="text-center mt-3 text-2xl">Mert Hasgeçkin</p>
+        <p class="text-center mt-3 text-2xl">Mert Hasgeçkin 1210606072</p>
         </li>
         <li>
-        <p class="text-center mt-3 text-2xl">Ali Yıldız</p>
+        <p class="text-center mt-3 text-2xl">Ali Yıldız 1200606013</p>
+        <p></p>
         </li>
         <li>
-        <p class="text-center mt-3 text-2xl">Fatih Atalay</p>
+        <p class="text-center mt-3 text-2xl">Fatih Atalay 1210606018</p>
         </li>
       </ul>
     </>
