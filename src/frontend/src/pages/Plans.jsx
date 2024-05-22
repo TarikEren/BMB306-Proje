a78f411c3d0ce24dea0cc9f26f0a4c6f586e023e
TarikEren <1210606015@nku.edu.tr>
2024-05-22 15:22:27 +0300

    Yeni plans sayfası

--- src/frontend/src/pages/Plans.jsx
+++ src/frontend/src/pages/Plans.jsx
@@ -9,40 +9,68 @@ function Plans() {
   return (
     <React.Fragment>
       <Navbar />
-  
-      <div class="w-screen h-screen m-10 ">
-        <div class="row h-screen">
-          <div class="ml-8 col-sm-5 h-screen">
-            <div class="card h-9/12 text-2xl">
-              <div class="card-body ">
-                <h5 class="card-title">Vip Paketi</h5>
-                <p class="card-text">- Sınırsız randevu hakkı </p>
-                <p class="card-text">- 7/24 Destek hattı</p>
-                <p class="card-text">-  35 TL </p>
-                <br></br>
-                <br></br>
-                <Link to="/payment" class="w-7/12 btn btn-primary" onClick={() => { setPrice(35); console.log(price) }}>Satın al</Link>
+      {/* Wrapper */}
+      <div class="w-screen h-screen">
+        {/* Container */}
+        <div className="mt-10 p-10 flex flex-row justify-around space-x-10">
+
+          {/* Plan 1*/}
+          <div className="border rounded p-3 w-7/12 flex flex-col justify-around">
+            {/* Başlık */}
+            <header className='flex flex-col space-y-3'>
+              <div className="text-center">
+                <h1 className='font-semibold text-4xl mb-3'>VIP Paketi</h1>
+                <h2 className='font-semibold text-3xl'>35 TL</h2>
               </div>
+              <hr />
+              <h2 className='font-semibold text-xl'>Avantajlar</h2>
+            </header>
+            {/* Avantajlar */}
+            <div className="mt-2 ml-5">
+              <ol className='list-disc'>
+                <li>
+                  <p>Sınırsız randevu hakkı</p>
+                </li>
+              </ol>
             </div>
+            {/* Satın Al */}
+            <footer className='flex flex-row justify-center items-center'>
+              <Link to="/payment" className='py-2 w-4/12 bg-blue-500 rounded text-white text-center' onClick={() => { setPrice(35) }} >
+                Satın Al
+              </Link>
+            </footer>
           </div>
-          <div class="col-sm-5 h-screen">
-            <div class="card ">
-              <div class="card-body text-2xl">
-                <h5 class="card-title text-2xl ">Destekçi Paketi</h5>
-                <p class="card-text text-2xl">- Destek icindir vip den farkı yoktur  </p>
-                <p class="card-text text-2xl">- Sınırsız ranevu hakkı </p>
-                <p class="card-text text-2xl">- 7/24 Destek hattı</p>
-                <p class="card-text text-2xl">- 60 TL</p>
-                <br></br>
-                <Link to="/payment" class="w-7/12 btn btn-primary" onClick={() => { setPrice(60) }}>Satın al</Link>
+
+          {/* Plan 2*/}
+          <div className="border rounded p-3 w-7/12 flex flex-col justify-around">
+            {/* Başlık */}
+            <header className='flex flex-col space-y-3'>
+              <div className="text-center">
+                <h1 className='font-semibold text-4xl mb-3'>Destekçi Paketi</h1>
+                <h2 className='font-semibold text-3xl'>60 TL</h2>
               </div>
+              <hr />
+              <h2 className='font-semibold text-xl'>Avantajlar</h2>
+            </header>
+            {/* Avantajlar */}
+            <div className="mt-2 ml-5">
+              <ol className='list-disc'>
+                <li>
+                  <p>Sınırsız randevu hakkı</p>
+                </li>
+                <li>
+                  <p>7/24 Destek hattı</p>
+                </li>
+              </ol>
             </div>
+            {/* Satın al */}
+            <footer className='flex flex-row justify-center items-center'>
+              <Link to="/payment" className='py-2 w-4/12 bg-blue-500 rounded text-white text-center' onClick={() => { setPrice(60) }}>
+                Satın Al
+              </Link>
+            </footer>
           </div>
         </div>
-
-
-
-
       </div>
     </React.Fragment>
   );
