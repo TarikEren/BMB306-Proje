3d56f4a64a136d3224de4f54d613d389e182b5d6
TarikEren <1210606015@nku.edu.tr>
2024-05-22 15:21:31 +0300

    Renk bugu düzeltildi

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,12 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    //Because we made a dynamic class with the label we need to add those clases
+    // to the safe list so the purge does not remove that
+    safelist: [
+      ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+      ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+      ...labelsClasses.map((lbl) => `text-${lbl}-400`)
+    ],
+  },
+  darkMode: "media",
   theme: {
+    extend: {
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr"
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
 }
-
