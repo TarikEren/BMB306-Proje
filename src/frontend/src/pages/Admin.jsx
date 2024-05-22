3179ccf3d1a6c6d76819830a1fc887ebffed2ee7
TarikEren <1210606015@nku.edu.tr>
2024-05-22 13:58:49 +0300

    Kullanıcı bilgileri alındı

--- src/frontend/src/pages/Admin.jsx
+++ src/frontend/src/pages/Admin.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import Navbar from '../components/Navbar';
 
 function Index() {
+
+    const [allUsers, setAllUsers] = useState([]);
+
+    useEffect(() => {
+        const getUserData = async () => {
+            await axios.get("http://localhost:8080/user")
+                .then((res) => {
+                    const userData = res.data;
+                    setAllUsers(userData);
+                })
+                .catch((err) => console.error(err));
+        }
+        getUserData();
+    }, []);
+    
+    useEffect(() => {
+        console.log(allUsers);
+    }, [allUsers]);
+
     return (
         <React.Fragment>
             <Navbar />
