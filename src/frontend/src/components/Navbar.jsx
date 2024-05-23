285c1d35715e1b1c54699a5b74db3b9df87db071
TarikEren <1210606015@nku.edu.tr>
2024-05-23 04:39:23 +0300

    Giriş yapan kullanıcının türüne göre değişik bağlantı gösterildi

--- src/frontend/src/components/Navbar.jsx
+++ src/frontend/src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import GlobalContext from '../context/GlobalContext';
 import logo from "../assets/logo.png";
 
 function Navbar() {
-    const { userLoggedIn, accountType } = useContext(GlobalContext);
+    const { userLoggedIn } = useContext(GlobalContext);
+    let accountType = localStorage.getItem("accountType");
     return (
         <nav className='flex flex-row bg-gradient-to-r from-teal-300 to-blue-400 drop-shadow-2xl px-4 py-2 text-white'>
             <div className="flex flex-row items-center">
                 <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
-                <a href="/" className='text-white py-2 text-xl'>Calendar</a>
+                <a href="/calendar" className='text-white py-2 text-xl'>Calendar</a>
             </div>
             <ul className='flex flex-row items-center justify-end w-full'>
                 <div className="flex flex-row items-center space-x-3">
@@ -16,7 +17,7 @@ function Navbar() {
                         <a href="/about" className='p-3 border rounded'>About</a>
                     </li>
                     <li className='p-2'>
-                        <a href="/plans" className='p-3 border rounded'>{ accountType === "premium" && userLoggedIn ? ("Plans") : ("Purchase")}</a>
+                        <a href="/plans" className='p-3 border rounded'>{ accountType === "premium" ? ("Plans") : ("Purchase")}</a>
                     </li>
                     <li className='p-2'>
                         <a href="/calendar" className='p-3 border rounded'>Calendar</a>
